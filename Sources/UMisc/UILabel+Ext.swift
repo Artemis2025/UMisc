db6a8331b76cfab51d2a4ec047d01e98f8c85fc3
Your Name <you@example.com>
2022-11-07 18:11:40 +0700

    fix adjustsFontForContentSizeCategory need iOS >= 10.0

--- Sources/UMisc/UILabel+Ext.swift
+++ Sources/UMisc/UILabel+Ext.swift
@@ -13,7 +13,9 @@ extension UILabel {
     public func uFont(_ textStyle: UIFont.TextStyle, auto: Bool = true){
         self.font = UIFont.preferredFont(forTextStyle: textStyle)
         if auto {
-            self.adjustsFontForContentSizeCategory = true
+            if #available(iOS 10.0, *) {
+                self.adjustsFontForContentSizeCategory = true
+            }
         }
     }
     
